fd892d95a36335709bf551fb35cdd97cf5f30753
Taylor Swain <taylorswain@Taylors-iMac.local>
2024-05-13 17:44:00 -0600

    first commit

---
+++ mail/contact_me.php
@@ -0,0 +1,30 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name']) ||
+   empty($_POST['email']) ||
+   empty($_POST['phone']) ||
+   empty($_POST['message']) ||
+   !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    echo "No arguments Provided!";
+    return false;
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+// Create the email and send the message
+$to = 'taylorswain9@gmail.com';
+$email_subject = "Website Contact Form: $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@taylorswain9@gmail.com\n";
+$headers .= "Reply-To: $email_address";
+if(mail($to, $email_subject, $email_body, $headers)) {
+    echo "Message successfully sent";
+    return true;
+} else {
+    echo "Message could not be sent";
+    return false;
+}
+?>
